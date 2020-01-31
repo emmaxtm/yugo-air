76f2424b367378ae2b5579ea462e5ad6cc3bddcf
emmaxtm <58564845+emmaxtm@users.noreply.github.com>
2020-01-31 19:53:00 +0100

    my first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Aero klub Yugo Air";
+            ViewBag.Description = "Letite kao profesionalac i ostvarite svoj san!";
+            ViewBag.Keywords = "letenje, jedrilica, obuke i treninzi";
+            return View();
+        }
+
+       [HttpGet]
+        public ActionResult Obrazac()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Obrazac(Models.Kandidat rezultat)
+        {
+            return View("Rezultat", rezultat);
+        }
+        [HttpGet]
+        public ActionResult Obrazac2()
+        {
+            return View();
+        }
+        
+        
+        public ActionResult O_nama()
+        {
+            ViewBag.Title = "Aero klub Yugo Air";
+            ViewBag.Message = "O nama";
+
+            return View();
+        }
+    }
+}
